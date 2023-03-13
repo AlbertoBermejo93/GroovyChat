bf3719c005c052957ebf3a4d250919ff20f05f94
Alberto Bermejo <79146441+AlbertoBermejo93@users.noreply.github.com>
2023-03-13 11:57:57 +0100

    vue components

---
+++ assets/components/Left/Conversation.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- TODO; Add this class when conversation selected (active) -->
+            <!-- active text-white -->
+            <a class="list-group-item list-group-item-action  rounded-0">
+              <div class="media"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+                <div class="media-body ml-4">
+                  <div class="d-flex align-items-center justify-content-between mb-1">
+                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">25 Dec</small>
+                  </div>
+                  <p class="font-italic mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+              </div>
+            </a>
+
+</template>
+
+
+<script>
+    export default {}
+</script>
